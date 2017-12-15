7ba44124b50ce263ba36dd18372ffd92a9b06c49
yong.wei <yong.wei@ipinyou.com>
2017-12-15 18:08:31 +0800

    add logo

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-      <el-row>
-          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-              <el-menu-item index="1">首页</el-menu-item>
-              <el-menu-item index="2">关于</el-menu-item>
-          </el-menu>
-      </el-row>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-row>
+            <el-col :span="4">
+                <img src="../assets/logo.png" class="logo">
+            </el-col>
+            <el-col :span="16">
+                <el-menu-item index="1"><span class="tab">首页</span></el-menu-item>
+                <el-menu-item index="2"><span class="tab">博客</span></el-menu-item>
+                <el-menu-item index="3"><span class="tab">关于</span></el-menu-item>
+            </el-col>
+        </el-row>
+
+      </el-menu>
   </div>
 </template>
 
@@ -26,5 +33,12 @@
 
 
 <style solt-scope>
+.logo{
+    height: 50px;
+    padding-top: 5px;
+}
 
+.tab{
+    font-weight: bold;
+}
 </style>
